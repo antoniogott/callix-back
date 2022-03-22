ee00499540945d9133ea4283f7cf3bd4e4ba5aa5
Antonio Gottsfritz <antoniobgott@gmail.com>
2022-03-21 23:51:48 -0300

    environment port

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const fastifyCors = require('fastify-cors');
 
 fastify.register(fastifyCors);
 
-fastify.register(require('./routes/v1/launch.js'), { prefix: '/v1/launch' })
+fastify.register(require('./routes/v1/launch.js'), { prefix: '/v1/launch' });
 
-fastify.listen(3001, function (err, address) {
+fastify.listen(process.env.PORT || 3001, '0.0.0.0', function (err, address) {
     if (err) {
         fastify.log.error(err);
         process.exit(1);
